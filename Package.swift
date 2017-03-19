ad1ec2d57165b0ed6bbd23a7c3b3b69875fdd5f6
Parker Kirby <jkirby@boomtownroi.com>
2017-03-19 19:51:07 -0400

    added libbsd module

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
-        .Package(url:"https://github.com/vapor/jwt.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 0, minor: 6),
+        .Package(url:  "https://github.com/dudash/swift-package-clibbsd.git", majorVersion: 1)
     ],
     exclude: [
         "Config",

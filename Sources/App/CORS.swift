fd6ac79b6bbe69e4dde6133ef435b9fccfe4ae0f
Parker Kirby <jkirby@boomtownroi.com>
2017-04-09 22:24:49 -0400

    added CORS support #16

--- Sources/App/CORS.swift
+++ Sources/App/CORS.swift
@@ -20,7 +20,7 @@ class CORS: Middleware {
         }
         
         response.headers["Access-Control-Allow-Origin"] = "*";
-        response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, Origin, Content-Type, Accept, Authorization"
+        response.headers["Access-Control-Allow-Headers"] = "Origin, Content-Type, Accept, Authorization"
         response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE, PATCH"
         return response
     }
